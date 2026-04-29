53d81c19af6a19239f30a70715e7f320b413bed4
phase <64127681+phasephasephase@users.noreply.github.com>
2026-04-29 11:45:31 -0600

    slightly unscuff the page pos & sizing

--- src/j3/window/main_window.cpp
+++ src/j3/window/main_window.cpp
@@ -5,14 +5,11 @@
 #include "j3/view/mods.hpp"
 #include "j3/view/settings.hpp"
 #include "j3/view/sidebar.hpp"
-#include "j3/view/test_place.hpp"
 #include "j3/view/versions.hpp"
 
 main_window::main_window(HINSTANCE instance, const std::wstring& title, const vector2 size, const bool main_window)
     : window(instance, title, size, main_window) {
     this->set_background_color({ 0.058f, 0.058f, 0.058f, 1 });
-
-    this->rml.register_view<test_place>();
     
     this->rml.register_view<home>();
     this->rml.register_view<mods>();
